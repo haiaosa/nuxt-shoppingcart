81d8a66eff90c69d5172538187ba9fdc551bf659
haiaosa <labatvii@gmail.com>
2019-03-22 11:41:10 +0700

    finished checkout function

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -4,7 +4,14 @@
     <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" alt="" />
     <ul v-else>
       <li v-for="product in products">
-        {{ product.title }} - {{ product.price }}
+        {{ product.title }} - {{ product.price | currency }} -
+        {{ product.inventory }}
+        <button
+          :disabled="!productIsInStock(product)"
+          @click="addProductToCart(product)"
+        >
+          Add to Cart
+        </button>
       </li>
     </ul>
   </div>
@@ -15,18 +22,27 @@ export default {
   data: function() {
     return {
       loading: false
-    };
+    }
   },
   computed: {
     products() {
-      return this.$store.getters.availableProducts;
+      return this.$store.state.products
+    },
+
+    productIsInStock() {
+      return this.$store.getters.productIsInStock
+    }
+  },
+  methods: {
+    addProductToCart(product) {
+      this.$store.dispatch('addProductToCart', product)
     }
   },
   created() {
-    this.loading = true;
-    this.$store.dispatch("fetchProducts").then(() => (this.loading = false));
+    this.loading = true
+    this.$store.dispatch('fetchProducts').then(() => (this.loading = false))
   }
-};
+}
 </script>
 
 <style scoped></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,46 @@
-import Vuex from "vuex";
-import Vue from "vue";
-import shop from "@/api/shop";
+import Vuex from 'vuex'
+import Vue from 'vue'
+import shop from '@/api/shop'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     // data
-    products: []
+    products: [],
+    cart: [],
+    checkoutStatus: null
   },
   getters: {
     // computed properties
     availableProducts(state, getters) {
-      return state.products.filter(product => product.inventory > 0);
+      return state.products.filter(product => product.inventory > 0)
+    },
+
+    cartProducts(state) {
+      return state.cart.map(cartItem => {
+        const product = state.products.find(
+          product => product.id === cartItem.id
+        )
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity
+        }
+      })
+    },
+
+    cartTotal(state, getters) {
+      return getters.cartProducts.reduce(
+        (total, product) => total + product.price * product.quantity,
+        0
+      )
+    },
+
+    productIsInStock() {
+      return product => {
+        return product.inventory > 0
+      }
     }
   },
   actions: {
@@ -22,11 +50,11 @@ export default new Vuex.Store({
       // run setProducts mutation
       return new Promise((resolve, reject) => {
         shop.getProducts(products => {
-          commit("setProducts", products);
-          resolve();
-        });
-      });
-    }
+          commit('setProducts', products)
+          resolve()
+        })
+      })
+    },
     // addToCart(context, product) {
     //   if(product.inventory > 0) {
     //     context.commit('pushProductToCart', product)
@@ -34,12 +62,61 @@ export default new Vuex.Store({
     //     // show out of stock message
     //   }
     // }
+    addProductToCart({ state, getters, commit }, product) {
+      if (getters.productIsInStock(product)) {
+        const cartItem = state.cart.find(item => item.id === product.id)
+        if (!cartItem) {
+          //push product to cart
+          commit('pushProductToCart', product.id)
+        } else {
+          // increment item quantity
+          commit('incrementItemQuantity', cartItem)
+        }
+        commit('decrementProductInventory', product)
+      }
+    },
+
+    checkout({ state, commit }) {
+      shop.buyProducts(
+        state.cart,
+        () => {
+          commit('emptyCart')
+          commit('setCheckoutStatus', 'success')
+        },
+        () => {
+          commit('setCheckoutStatus', 'fail')
+        }
+      )
+    }
   },
   mutations: {
     // setting or update state
     setProducts(state, products) {
       // update products
-      state.products = products;
+      state.products = products
+    },
+
+    pushProductToCart(state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+
+    incrementItemQuantity(state, cartItem) {
+      cartItem.quantity++
+    },
+
+    decrementProductInventory(state, product) {
+      product.inventory--
+    },
+
+    setCheckoutStatus(state, status) {
+      state.checkoutStatus = status
+    },
+
+    emptyCart(state) {
+      state.cart = []
     }
   }
-});
+})
